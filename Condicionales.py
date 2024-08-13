002d454b8c5f896d21cbee00b96f64be8c479a7e
larepo <larepo@gmail.com>
2024-08-13 13:55:34 -0400

    Prueba GitHub

--- Condicionales.py
+++ Condicionales.py
@@ -100,6 +100,8 @@ while True:
             round+=1
             continue
 
+#Prueba GITHUB
+
 
 #Para tributar un determinado impuesto se debe ser mayor de 16 años y tener unos ingresos iguales o superiores a 1000 € mensuales. 
 #Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales y muestre por pantalla si el usuario tiene que tributar o no.
