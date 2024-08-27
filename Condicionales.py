8733a465b00210f4f622b96525ae95882a6da872
larepo <larepo@gmail.com>
2024-08-27 14:12:46 -0400

    Inclusion de Conjuntos

--- Condicionales.py
+++ Condicionales.py
@@ -59,7 +59,7 @@ else:
     print ('#'*70)
 '''
 #Programa basico cachipun
-
+'''
 import random
 opc = ['a', 'b', 'c']
 round = 1
@@ -69,12 +69,12 @@ while True:
     print('*'*10)
     print('Soy Un Imbecil '*10)
 
-    print(''' ****   Vamos a jugar a skibidi toilet tycoon ****
+    print(' ****   Vamos a jugar a skibidi toilet tycoon ****
         Seleciona una de las siguientes opciones:
         a = PIEDRA
         b = PAPEL
         c = TIJERAS
-        ''') 
+        ') 
     Pc = random.choice (opc)
     user= input('Selecciona tu opcion a,b,c   =====>>: ')
     user=user.lower()
@@ -106,7 +106,7 @@ while True:
     elif opcion.upper() =='Y': #Hay que depurar (no condiciona correctamente )
             round+=1
             continue
-
+'''
 #Prueba GITHUB remoto
 
 
@@ -636,4 +636,78 @@ for filas in matriz:
         print(columnas)
 '''
         
-      
+#SETS (Conjuntos)
+#Se pueden modificar
+#No tienen un orden
+#No pueden tener elementos duplicados
+
+set_countries = {'Chile', 'Argentina', 'Brasil'} # no tiene un par key-value, así me doy cuenta que es un set, un conjunto.
+print(set_countries)
+print(type(set_countries))
+
+# si yo pongo algo repetido, él me lo quita al imprimir
+set_countries2 = {'Chile', 'Argentina', 'Brasil', 'Chile'}
+print (set_countries2) # {'Chile', 'Argentina', 'Brasil'}
+
+# puede ser mixto. El set se ordena solo, lo importante es lo que tengo dentro.
+set_types = {1, 'hola', False, 12.12}
+print(set_types) # {False, 1, 12.12, 'hola'}
+
+# la podemos crear a partir de un string
+set_from_string = set('hoola')
+print (set_from_string) # {'a', 'l', 'o', 'h'}
+set_from_string2 = set ('Conchetumare')
+print (set_from_string2)
+
+# la podemos crear a partir de una tupla
+set_from_tuples = set (('abc','cbv','as','abc'))
+print (set_from_tuples) # {'as', 'abc', 'cbv'}
+
+# la podemos crear a partir de una lista
+numbers = [1,2,3,1,2,3,4]
+set_numbers= set(numbers)
+print (set_numbers) # {1, 2, 3, 4}
+# si quiero convertir este set único a una lista, lo puedo hacer:
+unique_numbers = list(set_numbers)
+print (unique_numbers)
+
+#Funciones de set:
+#add(): Añade un elemento.
+#update(): Añade cualquier tipo de objeto iterable como: listas, tuplas.
+#discard(): Elimina un elemento y si ya existe no lanza ningún error.
+#remove(): Elimina un elemento y si este no existe lanza el error “keyError”.
+#pop(): Nos devuelve un elemento aleatorio y lo elimina y si el conjunto está vacío lanza el error “key error”.
+#clear(): Elimina todo el contenido del conjunto.
+
+
+set_countries = {'col', 'mex', 'bol'}
+
+#len() : Devuelve el tamaño del conjunto
+size = len(set_countries)
+print(size)
+
+#in, permite sabes si un elemento se encuentra en el conjunto, la expresión se evalua como true si el elemento se encuentra en el conjunto y false si el elemento no se encuentra en el conjunto
+print('col' in set_countries)
+print('pe' in set_countries)
+
+# add(): Añade un elemento al conjunto.
+set_countries.add('pe')
+print(set_countries)
+set_countries.add('pe')
+print(set_countries)
+
+# update(): Añade cualquier tipo de objeto iterable como: listas, tuplas
+set_countries.update({'ar', 'ecua', 'pe'})
+print(set_countries)
+
+# remove(): Elimina un elemento y si este no existe lanza el error “keyError”
+set_countries.remove('col')
+print(set_countries)
+set_countries.remove('ar')
+
+#discard(): Elimina un elemento y si ya existe no lanza ningún error
+set_countries.discard('arg')
+print(set_countries)
+set_countries.add('arg')
+print(set_countries)
+
